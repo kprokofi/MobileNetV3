3bf959f770f45df7d3c8e7fca44f07686a612d3b
kirill <kirill.prokofiev@intel.com>
2021-05-19 00:23:18 +0300

    fix resizing2

--- DataLoader.py
+++ DataLoader.py
@@ -182,7 +182,7 @@ def SVHNDataLoader(args):
 def CustomDatasetFolder(args):
     data_transforms = {
         'train': transforms.Compose([
-            transforms.Resize(224,224),
+            transforms.Resize((224,224)),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
