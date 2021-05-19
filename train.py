e475fd513e07f4c371c30a471c90c61c2ec629dd
kirill <kirill.prokofiev@intel.com>
2021-05-19 10:50:55 +0300

    debug

--- train.py
+++ train.py
@@ -16,6 +16,12 @@ from mobileNetV3 import MobileNetV3
 import argparse
 import copy
 from math import cos, pi
+import pickle
+import os.path as osp
+import warnings
+from collections import OrderedDict
+from functools import partial
+from pprint import pformat
 
 from statistics import *
 from EMA import EMA
@@ -24,6 +30,119 @@ from LabelSmoothing import LabelSmoothingLoss
 from ResultWriter import ResultWriter
 from CosineLR import *
 from Mixup import mixup_data, mixup_criterion
+
+def load_checkpoint(fpath):
+    r"""Loads checkpoint. Imported from openvinotoolkit/deep-object-reid.
+
+
+    Args:
+        fpath (str): path to checkpoint.
+
+    Returns:
+        dict
+    """
+    if fpath is None:
+        raise ValueError('File path is None')
+    if not osp.exists(fpath):
+        raise FileNotFoundError('File is not found at "{}"'.format(fpath))
+    map_location = None if torch.cuda.is_available() else 'cpu'
+    try:
+        checkpoint = torch.load(fpath, map_location=map_location)
+    except UnicodeDecodeError:
+        pickle.load = partial(pickle.load, encoding="latin1")
+        pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
+        checkpoint = torch.load(
+            fpath, pickle_module=pickle, map_location=map_location
+        )
+    except Exception:
+        print('Unable to load checkpoint from "{}"'.format(fpath))
+        raise
+    return checkpoint
+
+def _print_loading_weights_inconsistencies(discarded_layers, unmatched_layers):
+    if discarded_layers:
+        print(
+            '** The following layers are discarded '
+            'due to unmatched keys or layer size: {}'.
+            format(pformat(discarded_layers))
+        )
+    if unmatched_layers:
+        print(
+            '** The following layers were not loaded from checkpoint: {}'.
+            format(pformat(unmatched_layers))
+        )
+
+def check_isfile(fpath):
+    """Checks if the given path is a file.
+
+    Args:
+        fpath (str): file path.
+
+    Returns:
+       bool
+    """
+    isfile = osp.isfile(fpath)
+    if not isfile:
+        warnings.warn('No file found at "{}"'.format(fpath))
+    return isfile
+
+def load_pretrained_weights(model, file_path='', pretrained_dict=None, extra_prefix=''):
+    r"""Loads pretrianed weights to model. Imported from openvinotoolkit/deep-object-reid.
+    Features::
+        - Incompatible layers (unmatched in name or size) will be ignored.
+        - Can automatically deal with keys containing "module.".
+    Args:
+        model (nn.Module): network model.
+        file_path (str): path to pretrained weights.
+    """
+    def _remove_prefix(key, prefix):
+        prefix = prefix + '.'
+        if key.startswith(prefix):
+            key = key[len(prefix):]
+        return key
+
+    if file_path:
+        check_isfile(file_path)
+    checkpoint = (load_checkpoint(file_path)
+                       if not pretrained_dict
+                       else pretrained_dict)
+
+    if 'state_dict' in checkpoint:
+        state_dict = checkpoint['state_dict']
+    else:
+        state_dict = checkpoint
+
+    model_dict = model.state_dict()
+    new_state_dict = OrderedDict()
+    matched_layers, discarded_layers = [], []
+
+    for k, v in state_dict.items():
+        k = extra_prefix + _remove_prefix(k, 'module')
+
+        if k in model_dict and model_dict[k].size() == v.size():
+            new_state_dict[k] = v
+            matched_layers.append(k)
+        else:
+            discarded_layers.append(k)
+
+    model_dict.update(new_state_dict)
+    model.load_state_dict(model_dict)
+
+    message = file_path if file_path else "pretrained dict"
+    unmatched_layers = sorted(set(model_dict.keys()) - set(new_state_dict))
+    if len(matched_layers) == 0:
+        print(
+            'The pretrained weights "{}" cannot be loaded, '
+            'please check the key names manually'.format(message)
+        )
+        _print_loading_weights_inconsistencies(discarded_layers, unmatched_layers)
+
+        raise RuntimeError(f'The pretrained weights {message} cannot be loaded')
+    print(
+        'Successfully loaded pretrained weights from "{}"'.
+        format(message)
+    )
+    _print_loading_weights_inconsistencies(discarded_layers, unmatched_layers)
 
 def train(args, model, dataloader, loader_len, criterion, optimizer, scheduler, use_gpu, epoch, ema=None, save_file_name='train.csv'):
     '''
@@ -391,8 +510,7 @@ if __name__ == '__main__':
 
     if args.resume:
         if os.path.isfile(args.resume):
-            print(("=> loading checkpoint '{}'".format(args.resume)))
-            model.load_state_dict(torch.load(args.resume), strict=False)
+            load_pretrained_weights(model, args.resume)
         else:
             print(("=> no checkpoint found at '{}'".format(args.resume)))
             exit()
